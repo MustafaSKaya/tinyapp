8c394df065e4060255b1f6e5b5ccff3c1261b905
MustafaSKaya <mustafakya88@hotmail.com>
2021-12-07 02:28:15 +0000

    progress made

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString(num) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < num; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+    characters.length));
+ }
+ return result;
+}
 
 app.set("view engine", "ejs");
 
@@ -18,10 +30,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const newShortURL = generateRandomString(6);
+  urlDatabase[newShortURL] = req.body.longURL;
+  res.redirect(`/urls/${newShortURL}`);
+  console.log(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
